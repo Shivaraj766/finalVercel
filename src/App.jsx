5b4e88f5325999df77a9598ac4e68837c4449064
shivaraj singh <shivarajsing13@gmail.com>
2025-06-13 18:09:18 +0530

    Added Vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,20 @@ import DigitalLibraryHome from './components/DigitalLibraryHome/home.jsx';
 import Branch from './components/Branch/branch.jsx';
 import YearSemesterSelection from './components/YearSelection/index.jsx';
 import Subject from './components/Subject/index.jsx';
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<DigitalLibraryHome />} />
-      <Route path="/branches" element={<Branch isVisible />} />
-      <Route path="/:branch" element={<YearSemesterSelection />} />
-      <Route path="/:branch/:yearId/:semId" element={<Subject />} />
-
-      {/* Catch-all 404 route */}
-      <Route path="*" element={<h1>404 not found</h1>} />
-      <Analytics />
-    </Routes>
-    
+    <>
+      <Routes>
+        <Route path="/" element={<DigitalLibraryHome />} />
+        <Route path="/branches" element={<Branch isVisible />} />
+        <Route path="/:branch" element={<YearSemesterSelection />} />
+        <Route path="/:branch/:yearId/:semId" element={<Subject />} />
+        <Route path="*" element={<h1>404 not found</h1>} />
+      </Routes>
+      <Analytics /> 
+    </>
   );
 }
 
